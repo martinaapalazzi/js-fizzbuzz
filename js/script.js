23dce7f5228299907fb53fc3aace22bd3895d4e3
martinaapalazzi <martinaapalazzi@gmail.com>
2023-12-05 15:39:06 +0100

    edit ifs and else ifs

--- js/script.js
+++ js/script.js
@@ -17,22 +17,30 @@ Consigli del giorno:
 
 */ 
 
-let number = 1;
+let number;
 
 for (number = 1; number <= 100; number++) {
 
-    console.log ( number );
+    if ( number % 3 == 0 && number % 5 == 0 ) {
+        console.log ( 'FizzBuzz' );
+    } 
+
+    else if ( number % 3 == 0 ) {
+        console.log ( 'Fizz' );
+    }
+
+    else if ( number % 5 == 0 ) {
+        console.log ( 'Buzz' );
+    }
+    
+    else {
+        console.log (number);
+    }
+
+    
 }
 
-if ( number % 3 == 0 ) {
-    console.log ( number = Fizz );
-}
 
-else if ( number % 5 == 0 ) {
-    console.log ( number = Buzz );
-}
 
-else if ( number % 3 && number % 5 == 0 ) {
-    console.log ( number = FizzBuzz );
-}
+
 
