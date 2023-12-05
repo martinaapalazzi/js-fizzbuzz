a162a981812724f18adc614ffd081a6ba10e724c
martinaapalazzi <martinaapalazzi@gmail.com>
2023-12-05 14:36:20 +0100

    Inizializzazione esercizio

---
+++ js/script.js
@@ -0,0 +1,22 @@
+ /*
+
+Consegna:
+
+- Scrivi un programma che stampi in console i numeri da 1 a 100.
+- Per i multipli di 3 stampi "Fizz".
+- Per i multipli di 5 stampi "Buzz".
+- Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
+
+Domande:
+- Come faccio a sapere se un numero è divisibile per un altro?
+- Abbiamo visto qualcosa di particolare che possiamo usare?
+
+Consigli del giorno:
+1. Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare.
+2. Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano".
+
+*/ 
+
+let number = 1;
+
+for ( number = 1, number <= 100, number++ )
